b5c8e75f3dc7ba11f9ca426d5ac4de688627b752
jrmoore117 <jrmoore117@gmail.com>
2020-07-19 19:10:57 -0700

    Add basic page components with links and routes for each page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Router, Link } from "@reach/router";
+import HomePage from './pages/HomePage';
+import ButtonPage from './pages/ButtonPage';
 
 function App() {
   return (
     <div className="dashboard">
       <div className="sidebar">
-        sidebar
+        <Link to="/">Home</Link>
+        <Link to="/button">Button</Link>
       </div>
       <div className="content">
-        content
+        <Router>
+          <HomePage path="/"/>
+          <ButtonPage path="/button"/>
+        </Router>
       </div>
     </div>
   );
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => (
+   <div>
+      Button Page
+   </div>
+)
+
+export default Button;
