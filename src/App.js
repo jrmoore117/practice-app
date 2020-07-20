853293e0288e75b92e5f8297f2760d9ea18b031a
jrmoore117 <jrmoore117@gmail.com>
2020-07-19 19:01:51 -0700

    Add basic css grid for layout with sidebar.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      Hello world!
+    <div className="dashboard">
+      <div className="sidebar">
+        sidebar
+      </div>
+      <div className="content">
+        content
+      </div>
     </div>
   );
 }
