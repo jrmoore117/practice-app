1c26283ccb0556999c7a8a96634f7d7aac2ceb5a
jrmoore117 <jrmoore117@gmail.com>
2020-07-21 18:50:32 -0700

    Restructure pages directory to make imports easier.

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Button = () => (
+export const ButtonPage = () => (
    <div>
       Button Page
    </div>
 )
-
-export default Button;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Home = () => (
+export const HomePage = () => (
    <div>
-      Home page
+      <h1 className="circular">
+         Getting Started
+      </h1>
+      <p className="open" style={{"padding-right": "100px"}}>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cum vitae quasi iste quisquam ipsum ea totam magni tempora facere, maxime esse illum voluptatum quo consequuntur aut explicabo numquam. Officiis!
+      </p>
    </div>
 )
-
-export default Home;
