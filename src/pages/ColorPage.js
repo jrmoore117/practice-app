3d5433afc48d08ff69c9ac2d408f1669757803a4
jrmoore117 <jrmoore117@gmail.com>
2020-07-23 08:13:54 -0700

    Add ColorTiles to show each example color.

--- src/pages/ColorPage.js
+++ src/pages/ColorPage.js
@@ -4,5 +4,7 @@ import { ColorTile } from '../components';
 export const ColorPage = () => (
    <div>
       <ColorTile />
+      <ColorTile color="#F4D2D2" />
+      <ColorTile color="#F49FB6" />
    </div>
 )
