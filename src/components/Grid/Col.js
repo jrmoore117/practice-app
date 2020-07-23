99cc25576d5933b1c51e64d3ad562e46cfe1ac21
jrmoore117 <jrmoore117@gmail.com>
2020-07-22 22:59:58 -0700

    Remove commented-out component.

--- src/components/Grid/Col.js
+++ src/components/Grid/Col.js
@@ -1,19 +1,4 @@
 import React from 'react';
-// import './grid.css';
-
-// export function Col({ padding, id, className, children }) {
-//    return (
-//       <div
-//          id={id}
-//          className={className}
-//          style={{
-//             padding: `${padding || "0"}`
-//          }}
-//       >
-//          {children}
-//       </div>
-//    );
-// }
 
 export const Col = ({ columns, children }) => (
    <div
