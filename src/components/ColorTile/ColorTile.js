b8e5c343e6317e34d2da664fba64251a57146499
jrmoore117 <jrmoore117@gmail.com>
2020-07-24 17:16:16 -0700

    Add new color palette to color page.

--- src/components/ColorTile/ColorTile.js
+++ src/components/ColorTile/ColorTile.js
@@ -5,9 +5,16 @@ export const ColorTile = ({ size, color }) => (
    <div 
       className="colorTile"   
       style={{
+         margin: '10px 100px 10px 0',
          width: `${size || '50px'}`,
          height: `${size || '50px'}`,
-         backgroundColor: `${color || '#A6E0DE'}`
+         color: '#FAF8F0',
+         backgroundColor: `${color || '#A6E0DE'}`,
+         display: 'flex',
+         justifyContent: 'center',
+         alignItems: 'center'
       }}
-   />
+   >
+      {color}
+   </div>
 )
--- src/pages/ColorPage.js
+++ src/pages/ColorPage.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { ColorTile } from '../components';
 
 export const ColorPage = () => (
-   <div>
-      <ColorTile />
-      <ColorTile color="#F4D2D2" />
-      <ColorTile color="#F49FB6" />
+   <div style={{ display: 'flex' }}>
+      <div>
+         <ColorTile color="#A6E0DE" size="100px" />
+         <ColorTile color="#F4D2D2" size="100px" />
+         <ColorTile color="#F49FB6" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#85C4CB" size="100px" />
+         <ColorTile color="#5068A8" size="100px" />
+         <ColorTile color="#E74632" size="100px" />
+         <ColorTile color="#F88C99" size="100px" />
+         <ColorTile color="#FFB923" size="100px" />
+      </div>
    </div>
 )
