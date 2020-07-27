9ba9268d0e2c5dc2b61a36e129310812de06fb3e
jrmoore117 <jrmoore117@gmail.com>
2020-07-26 18:01:52 -0700

    Add hook logo svg.

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ function App() {
         <div
           className="circular" 
           style={{
-            height: "50px",
+            height: "70px",
+            width: '100vw',
             display: 'flex',
             alignItems: 'center',
-            color: 'black',
-            fontSize: '30px'
+            fontSize: '30px',
+            boxShadow: '0 2px 2px 0 rgb(240, 240, 240)'
           }}
         >
+          {/* <img style={{ height: '35px', margin: '0 10px 0 20px' }} src="/assets/images/tackle.svg"/> */}
+          <img style={{ height: '35px', margin: '0 10px 0 20px' }} src="/assets/images/tackle-mar.svg"/>
           tackle
         </div>
       </Row>
