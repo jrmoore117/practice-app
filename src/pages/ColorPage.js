99df75491c1839783418d8596d887606174de879
jrmoore117 <jrmoore117@gmail.com>
2020-07-26 18:05:02 -0700

    Add color page and remove circular font.

--- src/pages/ColorPage.js
+++ src/pages/ColorPage.js
@@ -4,16 +4,55 @@ import { ColorTile } from '../components';
 export const ColorPage = () => (
    <div style={{ display: 'flex' }}>
       <div>
-         <ColorTile color="#A6E0DE" size="100px" />
-         <ColorTile color="#F4D2D2" size="100px" />
-         <ColorTile color="#F49FB6" size="100px" />
+         <ColorTile color="#F3F0E2" size="100px" />
+         <ColorTile color="#F7F5EB" size="100px" />
+         <ColorTile color="#F9F7F1" size="100px" />
       </div>
       <div>
-         <ColorTile color="#85C4CB" size="100px" />
-         <ColorTile color="#5068A8" size="100px" />
-         <ColorTile color="#E74632" size="100px" />
-         <ColorTile color="#F88C99" size="100px" />
-         <ColorTile color="#FFB923" size="100px" />
+         <ColorTile color="#383838" size="100px" />
+         <ColorTile color="#7D887A" size="100px" />
+         <ColorTile color="#DADAD2" size="100px" />
+         <ColorTile color="#FFFFFF" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#042A38" size="100px" />
+         <ColorTile color="#05394C" size="100px" />
+         <ColorTile color="#096586" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#2E364F" size="100px" />
+         <ColorTile color="#2D5D7C" size="100px" />
+         <ColorTile color="#4186B4" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#10455B" size="100px" />
+         <ColorTile color="#2AA1AF" size="100px" />
+         <ColorTile color="#E2F0F1" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#22443B" size="100px" />
+         <ColorTile color="#2F6053" size="100px" />
+         <ColorTile color="#438975" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#FAB038" size="100px" />
+         <ColorTile color="#FBC774" size="100px" />
+         <ColorTile color="#FDDFB0" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#ED431D" size="100px" />
+         <ColorTile color="#EF5939" size="100px" />
+         <ColorTile color="#F57B51" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#A6191F" size="100px" />
+         <ColorTile color="#D52029" size="100px" />
+         <ColorTile color="#E29F94" size="100px" />
+      </div>
+      <div>
+         <ColorTile color="#760933" size="100px" />
+         <ColorTile color="#9B0C43" size="100px" />
+         <ColorTile color="#D1105A" size="100px" />
       </div>
    </div>
 )
