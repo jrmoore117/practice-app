668cecf6223b27a15aea850c174862a5e2e0bc52
jrmoore117 <jrmoore117@gmail.com>
2020-07-28 08:27:32 -0700

    Add list of study topics.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,5 +2,10 @@ import React from 'react';
 
 export const HomePage = () => (
    <div>
+      <ul>
+         {['GraphQL', 'Testing', 'Webpack', 'Basics of Python, Go, or Java', 'Context API', 'Common Hooks', 'Web Protocols, Cookies, etc.', 'Web Server Optimization', 'Caching', 'Angular'].map(item => (
+            <li key={Math.random()}>{item}</li>
+         ))}
+      </ul>
    </div>
 )
