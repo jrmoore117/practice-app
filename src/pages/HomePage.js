059952cfd03a911ad034da27098c2caf713acd6b
jrmoore117 <jrmoore117@gmail.com>
2020-08-03 23:34:38 -0700

    Change homepage content.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,19 @@
+/* @jsx jsx */
 import React from 'react';
+import { css, jsx } from '@emotion/core';
 
 export const HomePage = () => (
    <div>
-      <ul>
-         {['GraphQL', 'Testing', 'Webpack', 'Basics of Python, Go, or Java', 'Context API', 'Common Hooks', 'Web Protocols, Cookies, etc.', 'Web Server Optimization', 'Caching', 'Angular'].map(item => (
-            <li key={Math.random()}>{item}</li>
-         ))}
-      </ul>
+      <div>
+         <h2>Hooks</h2>
+         <h4>useState</h4>
+         <p>You can read more about the useState hook <a href='https://reactjs.org/docs/hooks-state.html'>here</a>.</p>
+      </div>
    </div>
 )
+
+{/* <ul>
+   {['GraphQL', 'Testing', 'Webpack', 'Basics of Python, Go, or Java', 'Context API', 'Common Hooks', 'Web Protocols, Cookies, etc.', 'Web Server Optimization', 'Caching', 'Angular'].map(item => (
+      <li key={Math.random()}>{item}</li>
+   ))}
+</ul> */}
