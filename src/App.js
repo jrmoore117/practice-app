a7c81afc03d46f2be6af3d3f36aa1f7f49f836b4
jrmoore117 <jrmoore117@gmail.com>
2020-08-03 22:31:42 -0700

    Swap in Navbar component. Replace inline styles withemotion css.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
+/* @jsx jsx */
 import React from 'react';
+import { css, jsx } from '@emotion/core';
 import { Router } from "@reach/router";
 import { HomePage, ButtonPage, ColorPage } from './pages';
-import { Row, Col, SidebarButton, SidebarTitle } from './components';
+import { Row, Col, SidebarButton, SidebarTitle, Navbar } from './components';
 
 function App() {
   return (
-    <div>
-      <div style={{height: '100vh', width: 'auto', backgroundImage: "url('/assets/images/landing-2.svg')"}} />
+    <div css={css`min-height: 100vh;`}>
       <Row>
-        <div
-          className="circular" 
-          style={{
-            height: "70px",
-            width: '100vw',
-            display: 'flex',
-            alignItems: 'center',
-            fontSize: '40px',
-            fontWeight: '600',
-            boxShadow: '0 2px 2px 0 rgb(240, 240, 240)'
-          }}
-        >
-          <img style={{ height: '45px', margin: '0 0 0 20px' }} src="/assets/images/bob-shade.svg" alt="logo"/>
-          tackle
-        </div>
+        <Navbar />
       </Row>
       <Row>
         <Col columns={2}>
