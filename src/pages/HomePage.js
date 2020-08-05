4b3ae91d1a65811de8caf793c281df4b568c38d5
jrmoore117 <jrmoore117@gmail.com>
2020-08-04 21:29:51 -0700

    Add practice examples with useState and useEffect.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,32 @@
 /* @jsx jsx */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { css, jsx } from '@emotion/core';
 
-export const HomePage = () => (
-   <div>
+const bold = css`font-weight: 700;`;
+
+export const HomePage = () => {
+   const [count, setCount] = useState(0);
+   const [time, setTime] = useState(new Date());
+   useEffect(() => {
+      document.title = `Count: ${count}`;
+      const timer = setTimeout(setTime(new Date()), 1000);
+      return () => clearTimeout(timer);
+   }, [count]);
+   return (
       <div>
-         <h2>Hooks</h2>
-         <h4>useState</h4>
-         <p>You can read more about the useState hook <a href='https://reactjs.org/docs/hooks-state.html'>here</a>.</p>
+         <div>
+            <h2>Hooks</h2>
+            <h4>useState: Counter</h4>
+            <p><span css={bold}>Count:</span> {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <h4>useEffect: Timer</h4>
+            <p><span css={bold}>Time:</span> {time.toLocaleTimeString()}</p>
+         </div>
       </div>
-   </div>
-)
+   );
+}
+
 
 {/* <ul>
    {['GraphQL', 'Testing', 'Webpack', 'Basics of Python, Go, or Java', 'Context API', 'Common Hooks', 'Web Protocols, Cookies, etc.', 'Web Server Optimization', 'Caching', 'Angular'].map(item => (
