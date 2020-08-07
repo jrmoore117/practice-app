75affe87b3bbdf6f3e8a1110ce5441cade1280b9
jrmoore117 <jrmoore117@gmail.com>
2020-08-06 20:41:33 -0700

    Add button to get a new joke.

--- src/components/examples/RandomJoke/RandomJoke.js
+++ src/components/examples/RandomJoke/RandomJoke.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 export const RandomJoke = () => {
    const [joke, setJoke] = useState(new Date());
-   useEffect(() => {
-      const getRandomJoke = async () => {
-         try {         
-            const result = await fetch('https://official-joke-api.appspot.com/jokes/random');
-            const jokeJSON = await result.json();
-            setJoke(jokeJSON);
-         } catch (err){
-            console.error(err);
-         }
+   const getRandomJoke = async () => {
+      try {         
+         const result = await fetch('https://official-joke-api.appspot.com/jokes/random');
+         const jokeJSON = await result.json();
+         setJoke(jokeJSON);
+      } catch (err){
+         console.error(err);
       }
+   }
+   useEffect(() => {
       getRandomJoke();
-   },[]);
+   }, []);
    return(
       <div>
          <h4>useEffect: Random Joke</h4>
          <p>{joke.setup}</p>
          <p>{joke.punchline}</p>
+         <button onClick={getRandomJoke}>Get New Joke</button>
       </div>
    );
 }
