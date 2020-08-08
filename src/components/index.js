3be5a4dd7d902b3b584b76c8067f986c06da2ed8
jrmoore117 <jrmoore117@gmail.com>
2020-08-08 13:50:00 -0700

    Update exports with new components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,15 @@
+export * from './Checkbox/Checkbox';
 export * from './ColorTile/ColorTile';
-export * from './examples/Counter/Counter';
 export * from './Flex/Flex';
-export * from './examples/Form/Form';
+export * from './Form/Form';
 export * from './Grid/Row';
 export * from './Grid/Col';
 export * from './Navbar/Navbar';
+export * from './Radio/Radio';
 export * from './SidebarButton/SidebarButton';
 export * from './SidebarTitle/SidebarTitle';
+export * from './TextInput/TextInput';
+
+export * from './examples/Counter/Counter';
 export * from './examples/RandomJoke/RandomJoke';
+export * from './examples/ExampleForm/ExampleForm';
