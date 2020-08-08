1093f59265fd1ea48bee803fbd963daa951159c5
jrmoore117 <jrmoore117@gmail.com>
2020-08-08 12:43:56 -0700

    Add exmaple checkbox, radio, and select inputs.

--- src/components/examples/Form/Form.js
+++ src/components/examples/Form/Form.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { useForm } from '../../../hooks';
 
 export const Form = () => {
-   const {form, bind, reset } = useForm({
+   const {form, bind, bindCheckbox, reset } = useForm({
       firstName: '',
       lastName: '',
-      email: ''
+      email: '',
+      promoEmails: false,
+      gender: '',
+      city: ''
    });
    const handleSubmit = (event) => {
       event.preventDefault();
@@ -19,6 +22,16 @@ export const Form = () => {
             <input type="text" placeholder="First Name" {...bind("firstName")} />
             <input type="text" placeholder="Last Name" {...bind("lastName")} />
             <input type="text" placeholder="Email" {...bind("email")} />
+            <input type="checkbox" {...bindCheckbox("promoEmails")}/>
+            <input type="radio" {...bind("gender", "male")} />
+            <input type="radio" {...bind("gender", "female")} />
+            <input type="radio" {...bind("gender", "other")} />
+            <select {...bind("city")}>
+               <option disabled value="">Select a city</option>
+               <option value="Rocklin">Rocklin</option>
+               <option value="Sacramento">Sacramento</option>
+               <option value="San Francisco">San Francisco</option>
+            </select>
             <input type="submit" value="submit" />
          </form>
       </div>
