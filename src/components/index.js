beb6fe49551b039c6a0fe4c5fffccccbc30f61fe
jrmoore117 <jrmoore117@gmail.com>
2020-08-08 11:30:52 -0700

    Update exports to include test form.

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export * from './ColorTile/ColorTile';
 export * from './examples/Counter/Counter';
 export * from './Flex/Flex';
+export * from './examples/Form/Form';
 export * from './Grid/Row';
 export * from './Grid/Col';
 export * from './Navbar/Navbar';
