389267d7b94cd5c9cd41c1d44fe83cc4b55a200a
jrmoore117 <jrmoore117@gmail.com>
2020-08-09 11:01:16 -0700

    Swap in new logo svg.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,11 +8,11 @@ export const Navbar = () => (
    width: 100vw;
    display: flex;
    align-items: center;
-   font-size: 40px;
+   font-size: 35px;
    font-weight: 600;
    box-shadow: 0 2px 2px 0 rgb(240, 240, 240);
  `}>
-      <img style={{ height: '45px', margin: '0 0 0 20px' }} src="/assets/images/bob-shade.svg" alt="logo" />
+      <img style={{ height: '45px', margin: '0 0 0 20px' }} src="/assets/images/bobber-5.svg" alt="logo" />
       <span>tackle</span>
    </div>
 )
