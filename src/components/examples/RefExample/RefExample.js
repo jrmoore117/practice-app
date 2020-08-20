23548a1778034da9ba7af6194ef71242de9de533
jrmoore117 <jrmoore117@gmail.com>
2020-08-19 21:12:31 -0700

    Swap ternary for short circuit in useForm.

--- src/components/examples/RefExample/RefExample.js
+++ src/components/examples/RefExample/RefExample.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const useForm = (initialFormState = {}) => {
    const [form, setForm] = useState(initialFormState);
@@ -6,49 +6,39 @@ const useForm = (initialFormState = {}) => {
       form,
       setForm,
       reset: () => setForm(initialFormState),
-      useBind: (name, value) => {
-         const input = useRef(null);
-         return {
-            ref: input,
-            name,
-            // can this be just - value || form[name]?
-            value: value ? value : form[name],
-            onChange: (event) => {
-               // trying to get the ability to add validation/other attribute changes through the ref
-               console.log(this.ref);
-               // const { name, value } = event.target;
-               // setForm(Object.assign({}, form, { [name]: value }));
-            }
-         }
-      },
-      bindCheckbox: (name) => ({
+      bind: (name, value) => ({
          name,
-         value: name,
-         checked: form[name],
+         value: value || form[name],
          onChange: (event) => {
-            const { checked } = event.target;
-            setForm(Object.assign({}, form, { [name]: checked }));
+            const { name, value } = event.target;
+            console.log(value);
+            setForm(Object.assign({}, form, { [name]: value }));
          }
       })
    };
 };
 
 export const RefExample = () => {
-
-   const {form, useBind, bindCheckbox, reset } = useForm()
-
-   // const element = useRef(null);
-   // const onClick = () => {
-   //    element.current.innerHTML === 'goodbye!'
-   //       ? element.current.innerHTML = 'hello!'
-   //       : element.current.innerHTML = 'goodbye!';
-   //    console.log(element.current);
-   // }
+   const {form, bind, reset } = useForm({
+      test: ''
+   })
+   const element = useRef(null);
+   const onClick = () => {
+      element.current.innerHTML === 'goodbye!'
+         ? element.current.innerHTML = 'hello!'
+         : element.current.innerHTML = 'goodbye!';
+      console.log(element.current);
+   }
    return(
       <div>
-         {/* <div ref={element}>Hello world!</div>
-         <button onClick={onClick}>Log Ref</button> */}
-         <input type="text" {...useBind("test")}/>
+         <div>
+            <div ref={element}>Hello world!</div>
+            <button onClick={onClick}>Log Ref</button>
+         </div>
+         <div>
+            <input type="text" {...bind("test")}/>
+            <button onClick={() => reset()}>Clear</button>
+         </div>
       </div>
    );
 }
--- src/hooks/useForm/useForm.js
+++ src/hooks/useForm/useForm.js
@@ -8,8 +8,7 @@ export const useForm = (initialFormState = {}) => {
       reset: () => setForm(initialFormState),
       bind: (name, value) => ({
          name,
-         // can this be just - value || form[name]?
-         value: value ? value : form[name],
+         value: value || form[name],
          onChange: (event) => {
             const { name, value } = event.target;
             setForm(Object.assign({}, form, { [name]: value }));
