12be7b149a13294de8864ea069deb89e2f0d6e46
jrmoore117 <jrmoore117@gmail.com>
2020-08-29 15:11:07 -0700

    Add prop for border.

--- src/components/ColorTile/ColorTile.js
+++ src/components/ColorTile/ColorTile.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { css, jsx } from '@emotion/core';
 
-export const ColorTile = ({ size, color }) => (
+export const ColorTile = ({ size, color, border }) => (
    <div 
       css={css`
          margin: 10px 10px 10px 0;
@@ -10,10 +10,11 @@ export const ColorTile = ({ size, color }) => (
          height: ${size || '50px'};
          color: #FFFFFF;
          background-color: ${color || '#FFFFFF'};
-         display: flex;
-         justify-content: center;
-         align-items: center;
+         border: ${border};
          border-radius: 8px;
+         display: flex;
+         align-items: center;
+         justify-content: center;
       `}
    >
       {color}
