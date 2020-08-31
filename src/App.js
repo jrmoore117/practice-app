2ed2177b3db337e76f5b5d85b0a6113403af5351
jrmoore117 <jrmoore117@gmail.com>
2020-08-30 19:29:25 -0700

    Add emotion ThemeProvider.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Router } from "@reach/router";
+import { ThemeProvider } from 'emotion-theming'
 import { HomePage, ComponentPage, ColorPage, ButtonPage, HooksPage } from './pages';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <HomePage path="/" />
-        <ComponentPage path="/components">
-          <HooksPage path="/" />
-          <ColorPage path="/color" />
-          <ButtonPage path="/button" />
-        </ComponentPage>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Router>
+          <HomePage path="/" />
+          <ComponentPage path="/components">
+            <HooksPage path="/" />
+            <ColorPage path="/color" />
+            <ButtonPage path="/button" />
+          </ComponentPage>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
