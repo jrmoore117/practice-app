2c8704f264f447b5faa2e32cc66ec199648fc4dd
jrmoore117 <jrmoore117@gmail.com>
2020-08-30 19:41:54 -0700

    Add random examples of Box and Button components.

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Button } from '../components';
+import { Box, Button } from '../components';
 
 export const ButtonPage = () => (
    <div>
-      <Button onClick={() => console.log('Button!')}>Save changes</Button>
-      <Button type="secondary" onClick={() => console.log('Button!')}>Save changes</Button>
+      <Button onClick={() => console.log('Button!')} shade="blue" mt={5}>Click me</Button>
+      <Box color={['white', 'red.5', 'blue.5']} bg="red.5">BOX</Box>
+      <Box color="white" bg="blue.5" cursor="pointer">BOX</Box>
+      <Box color="white" bg="purple.5">BOX</Box>
    </div>
 )
