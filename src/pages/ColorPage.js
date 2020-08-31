5c0f070d8f07f9eb8154a8ace18eb4cd338fe528
jrmoore117 <jrmoore117@gmail.com>
2020-08-30 19:39:51 -0700

    Swap in style-system colors - for example, theme.red['500'] was replaced with red.5

--- src/pages/ColorPage.js
+++ src/pages/ColorPage.js
@@ -1,120 +1,119 @@
 import React from 'react';
 import { ColorTile, Flex } from '../components';
-import theme from '../theme';
 
 export const ColorPage = () => (
    <div>
       {/* Gray */}
       <Flex>
-         <ColorTile color={theme.gray['0']} size="100px" />
-         <ColorTile color={theme.gray['100']} size="100px" />
-         <ColorTile color={theme.gray['200']} size="100px" />
-         <ColorTile color={theme.gray['300']} size="100px" />
-         <ColorTile color={theme.gray['400']} size="100px" />
-         <ColorTile color={theme.gray['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.gray['600']} size="100px" />
-         <ColorTile color={theme.gray['700']} size="100px" />
-         <ColorTile color={theme.gray['800']} size="100px" />
-         <ColorTile color={theme.gray['900']} size="100px" />
-         <ColorTile color={theme.gray['1000']} size="100px" />
+         <ColorTile color='gray.0' size="100px" />
+         <ColorTile color='gray.1' size="100px" />
+         <ColorTile color='gray.2' size="100px" />
+         <ColorTile color='gray.3' size="100px" />
+         <ColorTile color='gray.4' size="100px" />
+         <ColorTile color='gray.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='gray.6' size="100px" />
+         <ColorTile color='gray.7' size="100px" />
+         <ColorTile color='gray.8' size="100px" />
+         <ColorTile color='gray.9' size="100px" />
+         <ColorTile color='gray.10' size="100px" />
       </Flex>
       {/* Red */}
       <Flex>
-         <ColorTile color={theme.red['0']} size="100px" />
-         <ColorTile color={theme.red['100']} size="100px" />
-         <ColorTile color={theme.red['200']} size="100px" />
-         <ColorTile color={theme.red['300']} size="100px" />
-         <ColorTile color={theme.red['400']} size="100px" />
-         <ColorTile color={theme.red['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.red['600']} size="100px" />
-         <ColorTile color={theme.red['700']} size="100px" />
-         <ColorTile color={theme.red['800']} size="100px" />
-         <ColorTile color={theme.red['900']} size="100px" />
-         <ColorTile color={theme.red['1000']} size="100px" />
+         <ColorTile color='red.0' size="100px" />
+         <ColorTile color='red.1' size="100px" />
+         <ColorTile color='red.2' size="100px" />
+         <ColorTile color='red.3' size="100px" />
+         <ColorTile color='red.4' size="100px" />
+         <ColorTile color='red.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='red.6' size="100px" />
+         <ColorTile color='red.7' size="100px" />
+         <ColorTile color='red.8' size="100px" />
+         <ColorTile color='red.9' size="100px" />
+         <ColorTile color='red.10' size="100px" />
       </Flex>
       {/* Blue */}
       <Flex>
-         <ColorTile color={theme.blue['0']} size="100px" />
-         <ColorTile color={theme.blue['100']} size="100px" />
-         <ColorTile color={theme.blue['200']} size="100px" />
-         <ColorTile color={theme.blue['300']} size="100px" />
-         <ColorTile color={theme.blue['400']} size="100px" />
-         <ColorTile color={theme.blue['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.blue['600']} size="100px" />
-         <ColorTile color={theme.blue['700']} size="100px" />
-         <ColorTile color={theme.blue['800']} size="100px" />
-         <ColorTile color={theme.blue['900']} size="100px" />
-         <ColorTile color={theme.blue['1000']} size="100px" />
+         <ColorTile color='blue.0' size="100px" />
+         <ColorTile color='blue.1' size="100px" />
+         <ColorTile color='blue.2' size="100px" />
+         <ColorTile color='blue.3' size="100px" />
+         <ColorTile color='blue.4' size="100px" />
+         <ColorTile color='blue.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='blue.6' size="100px" />
+         <ColorTile color='blue.7' size="100px" />
+         <ColorTile color='blue.8' size="100px" />
+         <ColorTile color='blue.9' size="100px" />
+         <ColorTile color='blue.10' size="100px" />
       </Flex>
       {/* Purple */}
       <Flex>
-         <ColorTile color={theme.purple['0']} size="100px" />
-         <ColorTile color={theme.purple['100']} size="100px" />
-         <ColorTile color={theme.purple['200']} size="100px" />
-         <ColorTile color={theme.purple['300']} size="100px" />
-         <ColorTile color={theme.purple['400']} size="100px" />
-         <ColorTile color={theme.purple['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.purple['600']} size="100px" />
-         <ColorTile color={theme.purple['700']} size="100px" />
-         <ColorTile color={theme.purple['800']} size="100px" />
-         <ColorTile color={theme.purple['900']} size="100px" />
-         <ColorTile color={theme.purple['1000']} size="100px" />
+         <ColorTile color='purple.0' size="100px" />
+         <ColorTile color='purple.1' size="100px" />
+         <ColorTile color='purple.2' size="100px" />
+         <ColorTile color='purple.3' size="100px" />
+         <ColorTile color='purple.4' size="100px" />
+         <ColorTile color='purple.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='purple.6' size="100px" />
+         <ColorTile color='purple.7' size="100px" />
+         <ColorTile color='purple.8' size="100px" />
+         <ColorTile color='purple.9' size="100px" />
+         <ColorTile color='purple.10' size="100px" />
       </Flex>
       {/* Indigo */}
       <Flex>
-         <ColorTile color={theme.indigo['0']} size="100px" />
-         <ColorTile color={theme.indigo['100']} size="100px" />
-         <ColorTile color={theme.indigo['200']} size="100px" />
-         <ColorTile color={theme.indigo['300']} size="100px" />
-         <ColorTile color={theme.indigo['400']} size="100px" />
-         <ColorTile color={theme.indigo['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.indigo['600']} size="100px" />
-         <ColorTile color={theme.indigo['700']} size="100px" />
-         <ColorTile color={theme.indigo['800']} size="100px" />
-         <ColorTile color={theme.indigo['900']} size="100px" />
-         <ColorTile color={theme.indigo['1000']} size="100px" />
+         <ColorTile color='indigo.0' size="100px" />
+         <ColorTile color='indigo.1' size="100px" />
+         <ColorTile color='indigo.2' size="100px" />
+         <ColorTile color='indigo.3' size="100px" />
+         <ColorTile color='indigo.4' size="100px" />
+         <ColorTile color='indigo.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='indigo.6' size="100px" />
+         <ColorTile color='indigo.7' size="100px" />
+         <ColorTile color='indigo.8' size="100px" />
+         <ColorTile color='indigo.9' size="100px" />
+         <ColorTile color='indigo.10' size="100px" />
       </Flex>
       {/* Coral */}
       <Flex>
-         <ColorTile color={theme.coral['0']} size="100px" />
-         <ColorTile color={theme.coral['100']} size="100px" />
-         <ColorTile color={theme.coral['200']} size="100px" />
-         <ColorTile color={theme.coral['300']} size="100px" />
-         <ColorTile color={theme.coral['400']} size="100px" />
-         <ColorTile color={theme.coral['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.coral['600']} size="100px" />
-         <ColorTile color={theme.coral['700']} size="100px" />
-         <ColorTile color={theme.coral['800']} size="100px" />
-         <ColorTile color={theme.coral['900']} size="100px" />
-         <ColorTile color={theme.coral['1000']} size="100px" />
+         <ColorTile color='coral.0' size="100px" />
+         <ColorTile color='coral.1' size="100px" />
+         <ColorTile color='coral.2' size="100px" />
+         <ColorTile color='coral.3' size="100px" />
+         <ColorTile color='coral.4' size="100px" />
+         <ColorTile color='coral.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='coral.6' size="100px" />
+         <ColorTile color='coral.7' size="100px" />
+         <ColorTile color='coral.8' size="100px" />
+         <ColorTile color='coral.9' size="100px" />
+         <ColorTile color='coral.10' size="100px" />
       </Flex>
       {/* Yellow */}
       <Flex>
-         <ColorTile color={theme.yellow['0']} size="100px" />
-         <ColorTile color={theme.yellow['100']} size="100px" />
-         <ColorTile color={theme.yellow['200']} size="100px" />
-         <ColorTile color={theme.yellow['300']} size="100px" />
-         <ColorTile color={theme.yellow['400']} size="100px" />
-         <ColorTile color={theme.yellow['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.yellow['600']} size="100px" />
-         <ColorTile color={theme.yellow['700']} size="100px" />
-         <ColorTile color={theme.yellow['800']} size="100px" />
-         <ColorTile color={theme.yellow['900']} size="100px" />
-         <ColorTile color={theme.yellow['1000']} size="100px" />
+         <ColorTile color='yellow.0' size="100px" />
+         <ColorTile color='yellow.1' size="100px" />
+         <ColorTile color='yellow.2' size="100px" />
+         <ColorTile color='yellow.3' size="100px" />
+         <ColorTile color='yellow.4' size="100px" />
+         <ColorTile color='yellow.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='yellow.6' size="100px" />
+         <ColorTile color='yellow.7' size="100px" />
+         <ColorTile color='yellow.8' size="100px" />
+         <ColorTile color='yellow.9' size="100px" />
+         <ColorTile color='yellow.10' size="100px" />
       </Flex>
       {/* Green */}
       <Flex>
-         <ColorTile color={theme.green['0']} size="100px" />
-         <ColorTile color={theme.green['100']} size="100px" />
-         <ColorTile color={theme.green['200']} size="100px" />
-         <ColorTile color={theme.green['300']} size="100px" />
-         <ColorTile color={theme.green['400']} size="100px" />
-         <ColorTile color={theme.green['500']} size="100px" border="3px solid #262322" /> {/* 500 */}
-         <ColorTile color={theme.green['600']} size="100px" />
-         <ColorTile color={theme.green['700']} size="100px" />
-         <ColorTile color={theme.green['800']} size="100px" />
-         <ColorTile color={theme.green['900']} size="100px" />
-         <ColorTile color={theme.green['1000']} size="100px" />
+         <ColorTile color='green.0' size="100px" />
+         <ColorTile color='green.1' size="100px" />
+         <ColorTile color='green.2' size="100px" />
+         <ColorTile color='green.3' size="100px" />
+         <ColorTile color='green.4' size="100px" />
+         <ColorTile color='green.5' size="100px" border="3px solid #262322" /> {/* 500 */}
+         <ColorTile color='green.6' size="100px" />
+         <ColorTile color='green.7' size="100px" />
+         <ColorTile color='green.8' size="100px" />
+         <ColorTile color='green.9' size="100px" />
+         <ColorTile color='green.10' size="100px" />
       </Flex>
       <Flex>
          <ColorTile color="#db1d34" size="100px" />
