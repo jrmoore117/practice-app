4032281599900d74655bd05c61f69148b6c192c6
jrmoore117 <jrmoore117@gmail.com>
2020-08-31 23:32:39 -0700

    Remove unused shade prop.

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -3,8 +3,8 @@ import { Box, Button } from '../components';
 
 export const ButtonPage = () => (
    <div>
-      <Button onClick={() => console.log('Button!')} shade="blue" mt={5}>Click me</Button>
-      <Box color={['white', 'red.5', 'blue.5']} bg="red.5">BOX</Box>
+      <Button onClick={() => console.log('Button!')} variant="primary">Defaut Button</Button>
+      <Box color={['white', 'red.5', 'blue.5', 'white']} bg="red.5">BOX</Box>
       <Box color="white" bg="blue.5" cursor="pointer">BOX</Box>
       <Box color="white" bg="purple.5">BOX</Box>
    </div>
