766ef462548a69c20a69aeafe1aa0313980c764d
jrmoore117 <jrmoore117@gmail.com>
2020-09-22 23:41:51 -0700

    Remove tailwind class from buttonpage.

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Button } from '../components';
 
 export const ButtonPage = () => (
-   <div className="m-10">
+   <div>
       <Button onClick={() => console.log('Button!')} variant="primary">Defaut Button</Button>
       <Box color={['white', 'red.5', 'blue.5', 'white']} bg="red.5">BOX</Box>
       <Box color="white" bg="blue.5" cursor="pointer">BOX</Box>
